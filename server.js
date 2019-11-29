ad7579922609b197a1afac17cbd4f18e087c4fd8
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-11-29 01:50:12 +0100

    feat: added

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
-const app = express();
 
 const PORT = 3001;
 const HOST = '127.0.0.1';
 
 const sequelize = new Sequelize('', 'root', 'root', {
 	dialect: 'mysql',
-	port: 3309,
+	host: 'dataBase',
+	port: 3306,
 	pool: {
 		max: 10,
 		min: 0,
@@ -17,19 +16,18 @@ const sequelize = new Sequelize('', 'root', 'root', {
 	},
 });
 
-// to run on docker docker run -p 3001:3001 test-node-app
-// sequelize2.authenticate().then(function(errors) {
-// 	console.log(errors);
-// });
+sequelize.authenticate().then(function(errors) {
+	console.log(errors);
+});
 
-try {
-	sequelize
-		.sync()
-		.then(() => app.listen(PORT, () => console.log('running on port: ' + PORT)))
-		.catch(err => console.log(err));
+// try {
+// 	sequelize
+// 		.sync()
+// 		.then(() => app.listen(PORT, () => console.log('running on port: ' + PORT)))
+// 		.catch(err => console.log(err));
 
-	// when it breaks append this .authenticate() then run then delete the word
-	// it works by forcing to re-store the connection
-} catch (err) {
-	console.log('server err', err);
-}
+// 	// when it breaks append this .authenticate() then run then delete the word
+// 	// it works by forcing to re-store the connection
+// } catch (err) {
+// 	console.log('server err', err);
+// }
